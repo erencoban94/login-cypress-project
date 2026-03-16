7e9ed73476ec9604d4a85e9955eaeb850ff701bd
Han Software <erencoban94@hotmail.com>
2026-03-16 05:15:17 +0300

    chore: github url eklendi

---
+++ index.js
@@ -0,0 +1,14 @@
+function cypressTestProjesi() {
+  const checklist = {
+    // BURAYI DÜZENLE: Kendi GitHub linkini kopyalayıp buraya yapıştır.
+    // Örnek: 'https://github.com/erencoban94/Workingtech-project.git'
+    project_url:
+      'https://github.com/erencoban94/login-cypress-project',
+    // ⚠️  Yukarıdaki URL'i kendi repo adresinle güncellemeyi unutma!
+  };
+
+  return checklist;
+}
+
+/* Bu satırdan sonraki satırları silmeyin! */
+module.exports = cypressTestProjesi;
